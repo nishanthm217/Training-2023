f9f910c4448d6226d5d30cec9c3c328575b6ef63
nishanthm217 <113766678+nishanthm217@users.noreply.github.com>
2023-03-15 08:51:57 +0530

    Add files via upload

---
+++ Project/source code/onlinefood.js
@@ -0,0 +1,11 @@
+// const menuItems = [
+//     { name: "Burger", description: "A delicious burger with all the fixings", price: 10.00 },
+//     { name: "Pizza", description: "A classic pizza with your choice of toppings", price: 12.00 },
+//     { name: "Salad", description: "A healthy salad with fresh ingredients", price: 8.00 },
+//   ];
+  
+//   const cartItems = [];
+  
+//   const orderButtons = document.querySelectorAll(".order-btn");
+//   const cartItemsList =
+  
